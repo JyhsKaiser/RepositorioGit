0ed3ff7c03df274c237f5f6f0ce9073a574d3052
“Jovani <jyhskaiser@gmail.com>
2025-02-13 18:41:47 -0600

    Se agregan modificaciones adicionales a nuestro JFrame

--- src/repositoriogit/Ventana.java
+++ src/repositoriogit/Ventana.java
@@ -2,6 +2,7 @@ package repositoriogit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -11,16 +12,18 @@ import javax.swing.JTextField;
 public class Ventana extends JFrame
 {
 
+	JPanel panelMain;
 	JLabel nombre;
 	JTextField tnombre;
 
 	public Ventana()
 	{
 		setTitle("Ejemplo");
-		setSize(100, 300);
+		setSize(300, 300);
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);		
+		setResizable(false);
 		componentes();
 		setVisible(true);
 
@@ -28,14 +31,20 @@ public class Ventana extends JFrame
 
 	public void componentes()
 	{
+		panelMain = new JPanel();
+		panelMain.setBounds(300,300,300,300);
+		panelMain.setLayout(null);
+		
 		nombre = new JLabel("Nombre");
 		nombre.setBounds(50, 50, 100, 30);
 
 		tnombre = new JTextField();
 		tnombre.setBounds(150, 50, 100, 30);
 
+		add(panelMain);
 		add(nombre);
 		add(tnombre);
+		
 	}
 
 	public static void main(String[] args)
